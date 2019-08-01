de45f65d90d17f5973421715bf506c4474a37bc9
maycon.pereira <maycon.pereira@admoto.moto.com.br>
2019-08-01 19:25:33 -0300

    New project gerenciador de tarefas

---
+++ gerenciador-de-tarefas/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+export const routes: Routes = [
+// definição das rotas de navegação do component
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
+
+})
+
+export class AppRoutingModule {
+
+}
